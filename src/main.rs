636539a9d15f8022beaeb14114b67290d57129b6
Adam Dąbrowski <contact@adamdabrowski.com>
2018-01-14 23:56:32 +0100

    Implement: Moving around

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,27 @@ const SCREEN_WIDTH : i32 = 80;
 const SCREEN_HEIGHT : i32 = 50;
 const FPS_LIMIT : i32 = 20;
 
+fn handle_keys(root : &mut Root, player_x : &mut i32, player_y : &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        },
+        Key { code: Escape, .. } => return true,
+        _ => {},
+    }
+
+    false
+}
+
 fn main() {
     let mut root = Root::initializer()
         .title("Rust'n'Ruin")
@@ -17,10 +38,17 @@ fn main() {
 
     tcod::system::set_fps(FPS_LIMIT);
 
+
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
     while !root.window_closed() {
         root.set_default_foreground(colors::WHITE);
-        root.put_char(1, 1, '@', BackgroundFlag::None);
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         root.flush();
-        root.wait_for_keypress(true);
+        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit { break }
+
     }
 }
