511a13dbd8169315a772103b46bc59103c7fdf13
Adam Dąbrowski <contact@adamdabrowski.com>
2018-01-15 00:47:27 +0100

    Implement: Dungeon building blocks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate tcod;
 
+use std::cmp;
 use tcod::console::*;
 use tcod::colors;
 
@@ -27,6 +28,20 @@ impl Tile {
 
     pub fn wall() -> Self {
         Tile { blocked: true, block_sight: true }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1 : i32,
+    y1 : i32,
+    x2 : i32,
+    y2 : i32,
+}
+
+impl Rect {
+    pub fn new(x : i32, y : i32, w : i32, h : i32) -> Self {
+        Rect { x1: x, y1: y, x2: x + w, y2: y + h }
     }
 }
 
@@ -66,8 +81,34 @@ impl Entity {
 }
 
 fn make_map() -> Map {
-    let mut map = vec![vec![Tile::ground(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
+
     map
+}
+fn create_room(room : Rect, map : &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::ground();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::ground();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::ground();
+    }
 }
 
 fn render_all(root: &mut Root, con: &mut Offscreen, entities: &[Entity], map: &Map) {
@@ -127,9 +168,7 @@ fn main() {
     tcod::system::set_fps(FPS_LIMIT);
 
     let mut map = make_map();
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
-    let player = Entity::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let player = Entity::new(25, 23, '@', colors::WHITE);
     let npc = Entity::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, 'O', colors::YELLOW);
     let mut entities = [player, npc];
 
